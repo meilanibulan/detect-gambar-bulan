6790c2c8e96aafc3176a841fdc39680ff11ab4b3
meilanibulan <bulanmeilani6@gmail.com>
2025-10-25 00:02:40 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -53,7 +53,7 @@ st.markdown("""
 <style>
 /* --- SIDEBAR --- */
 [data-testid="stSidebar"]{
-  background: #a9def9!important;
+  background: #d9eff9 !important;
   border-right: 1px solid rgba(0,0,0,.06);
   width: 220px !important;
   min-width: 220px !important;
